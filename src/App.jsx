151eda06000221289f3976dd04939a7017e51be8
LuisPerez2008 <113653537+LuisPerez2008@users.noreply.github.com>
2025-12-10 00:42:28 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function App() {
+    return (
+        <div className="wrapper bg-principal h-screen w-full  text-white font-orbitron">
+            hola mundo
+        </div>
+    );
+}
+
+export default App;
