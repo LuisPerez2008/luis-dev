99cea7e5937008f5e64d3906bb48aa30c1e3656f
LuisPerez2008 <113653537+LuisPerez2008@users.noreply.github.com>
2025-12-17 00:13:48 -0500

    se agrego la secion de About informacion sobre mi

---
+++ src/components/AboutMe2.jsx
@@ -0,0 +1,42 @@
+export const AboutMe2 = () => {
+    return (
+        <section className="mt-32 mb-50 text-white">
+            <div className="max-w-[90%] mx-auto flex flex-col gap-10">
+                <div className="flex items-center gap-3 group ">
+                    <div className="relative flex items-center justify-center ">
+                        <span className="absolute inset-0 rounded-full group-hover:bg-morado-300 blur-xl  transition-all duration-500 group-hover:scale-150 group-hover:opacity-100 " />
+                        <svg className="w-7 h-7 text-morado-300 relative z-10 group-hover:scale-110 transition-all duration-300">
+                            <use href="/Icons.svg#user" />
+                        </svg>
+                    </div>
+
+                    <span className="font-orbitron whitespace-nowrap text-white w-full text-2xl font-bold relative inline-flex items-center gap-4 after:content-[''] after:block after:h-0.5 after:w-full after:bg-linear-to-r after:from-morado-600 after:to-transparent after:shadow-md   ">
+                        SOBRE MI
+                    </span>
+                </div>
+                <div className="relative">
+                    {/* FONDO DIFUMINADO */}
+                    <div className="absolute inset-0 bg-morado-500/40 blur-2xl rounded-xl -z-10" />
+                    <div className=" relative z-10 border border-morado-600 p-6 bg-morado-400/15 rounded-xl text-white backdrop-blur-xs hover:border-morado-700">
+                        <p>
+                            Ingeniero en sistemas, especializado en el <span className="text-font-primary">desarrollo FullSatck</span> con metodologias agiles 
+                        </p>
+                    </div>
+                </div>
+                {/*  <div className="">
+                    <div className="border border-morado-600 p-6 bg-morado-400/15  rounded-xl text-white blur-  relative z-50">
+                    
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Totam ratione laudantium at laboriosam
+                            officiis et natus ducimus aliquam odit nesciunt
+                            neque cum, commodi error ex qui magni, assumenda,
+                            veniam molestiae?
+                        </p>
+                    </div>
+                   
+                </div> */}
+            </div>
+        </section>
+    );
+};
--- src/pages/Principal.jsx
+++ src/pages/Principal.jsx
@@ -1,11 +1,13 @@
 import { AboutMe } from "../components/AboutMe"
+import { AboutMe2 } from "../components/AboutMe2"
 import { Header } from "../components/Header"
 
 export const Principal = () => {
   return (
-    <div className="h-screen w-full ">
+    <div className="h-auto w-full ">
       <Header />
       <AboutMe />
+      <AboutMe2 />
 
      
 
